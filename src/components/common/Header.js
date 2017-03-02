91de95456bbe97efbc185cb83fff777cb55305fb
Scott Enriquez <scottie@enriquez.io>
2017-03-01 21:48:14 -0600

    Properly configured Materialize and jQuery

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,11 +1,14 @@
 import React, {PropTypes} from 'react';
-import {IndexLink} from 'react-router';
+import {IndexLink, Link} from 'react-router';
 
 const Header = () => {
 	return(
-		<nav>
+		<nav className="orange darken-4">
 			<div className="nav-wrapper">
 				<IndexLink to="/" className="brand-logo">Scottie</IndexLink>
+				<ul className="right hide-on-med-and-down">
+					<li><Link to="/" className="waves-effect waves-light btn grey darken-4">Resume</Link></li>
+				</ul>
 			</div>
 		</nav>
 	);
--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import {render} from 'react-dom';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
 import './styles/styles.css';
+var $ = require('jquery');
+window.jQuery = $;
+window.$ = $;
 import '../node_modules/materialize-css/dist/css/materialize.min.css';
-import '../node_modules/materialize-css/dist/js/materialize.min';
+import '../node_modules/materialize-css/dist/js/materialize.min.js';
 
 render(
 	<Router history={browserHistory} routes={routes} />,
